4ee02698e16bbe08482bc384ab18931ea47c8590
x-x-l <ayhanaliyev@gmail.com>
2017-11-14 15:00:04 -0500

    when they are set, REDIS_HOST and REDIS_PORT environment variables are now given higher precedence than localhost for default values

--- default_worker_options.js
+++ default_worker_options.js
@@ -1,12 +1,12 @@
 module.exports = {
-  port: 6379,
-  host: "127.0.0.1",
-  maxConcurrency: 10,
-  popTimeout: 3, // 3 seconds
-  maxRetries: 10,
-  stalledTimeout: 5e3, // 5 seconds
-  runTimeoutWatchdog: true,
-  runStalledWatchdog: true,
-  stall: false,
-  autoListen: true
+    port: process.env.REDIS_PORT || 6379,
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    maxConcurrency: 10,
+    popTimeout: 3, // 3 seconds
+    maxRetries: 10,
+    stalledTimeout: 5e3, // 5 seconds
+    runTimeoutWatchdog: true,
+    runStalledWatchdog: true,
+    stall: false,
+    autoListen: true
 };
