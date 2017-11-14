5072adf3902625e4d3ea36f62f93361401c5761b
x-x-l <ayhanaliyev@gmail.com>
2017-11-14 15:19:53 -0500

    reverted

--- default_client_options.js
+++ default_client_options.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.REDIS_PORT || 6379,
-    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: 6379,
+    host: "127.0.0.1",
     defaultTimeout: 60e3
 };
--- default_worker_options.js
+++ default_worker_options.js
@@ -1,6 +1,6 @@
 module.exports = {
-    port: process.env.REDIS_PORT || 6379,
-    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: 6379,
+    host: "127.0.0.1",
     maxConcurrency: 10,
     popTimeout: 3, // 3 seconds
     maxRetries: 10,
